508435cef8010e0042c958db2e823037d5bcc29c
Telvin Varfley <telvinvarfley@gmail.com>
2021-10-24 14:25:05 +0000

    Level 3 Hashing

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require("express")
 const bodyParser = require("body-parser")
-const ejs = require('ejs')
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+// Level 3 included hashing
+const md5 = require("md5")
+// const encrypt = require("mongoose-encryption")
 const app = express()
 const port = 3000 || process.env.PORT
+
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
@@ -25,6 +27,7 @@ const options = {
 mongoose.connect(url, options)
 
 // Level one starts here
+// For level 2 to work i had to change
 const userSchema =new mongoose.Schema({
     email: String,
     password : String
@@ -36,9 +39,9 @@ const userSchema =new mongoose.Schema({
 // Check 
 // This encrypts the whole database.
 // But when you add a option then it encrypts it
-// The plugin her eis used to encrypt the password fir
-// Level 3 Environment Variable.
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password']});
+// The plugin her eis used to encrypt the password for
+// Level 2 Environment Variable.
+// userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema)
 
@@ -60,13 +63,15 @@ app.get('/secrets', (req, res)=>{
     res.render('secrets')
 })
 
-
+app.get('/submit', (req, res)=>{
+    res.render('submit')
+});
 // Post requests
 // This was just level  one 
 app.post("/register", function(req, res){
     const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
     })
 
     newUser.save(function(err){
@@ -81,7 +86,7 @@ app.post("/register", function(req, res){
 // Level one 
 app.post('/login', function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if(err){
